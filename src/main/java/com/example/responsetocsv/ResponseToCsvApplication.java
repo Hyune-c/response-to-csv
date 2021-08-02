3ed2508538e66553bd02d86e000c4f2153af79fc
choibyunghyeon <choi8608@gmail.com>
2021-08-02 12:23:16 +0900

    Initial commit

---
+++ src/main/java/com/example/responsetocsv/ResponseToCsvApplication.java
@@ -0,0 +1,13 @@
+package com.example.responsetocsv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResponseToCsvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResponseToCsvApplication.class, args);
+    }
+
+}
